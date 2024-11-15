c8160322e248abb8cfed5fe7cc1a0d311e3cbcb9
Awais142 <awaisakbar142@gmail.com>
2024-11-15 18:37:33 +0500

    New font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
       colors: {
         primary: "#050816",
         secondary: "#aaa6c3",
